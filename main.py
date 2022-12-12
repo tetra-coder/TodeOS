8aa764df6bfa9f29842bea29917a491db828102f
Abhinav Singh <abhinavumangsingh@gmail.com>
2022-12-12 20:49:33 +0530

    Fixed grammar mistake and added feedback for invalid commands

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from getpass import getpass
 from webbrowser import open_new_tab
 from os import system
 import hashlib
-
 isAdmin = False
+adminHash = 'b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86'
+def hashPassword(password):
+    return hashlib.sha512(password.encode("utf8")).hexdigest()
 system("cls")
 print("WARNING: UNDER CONSTRUCTION")
 print("Type help for a list of commands.")
