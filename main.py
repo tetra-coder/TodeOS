a5739e88b9e6528c0eddf64c6f382e91bd53469e
Abhinav Singh <abhinavumangsingh@gmail.com>
2022-12-23 21:25:25 +0530

    sudo rickroll, cd and improved imports

--- main.py
+++ main.py
@@ -1,33 +1,32 @@
 from System.passwords import hashPassword, adminHash
 from System.ls import ls
-from Games.ponggame import pong
-from getpass import getpass
-from webbrowser import open_new_tab
-from os import system
-from Games.eldenring import eldenring
+import Games
+import getpass
+import os
 isAdmin = False
 adminHash = 'b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86'
-system("cls")
+os.system("cls")
 print("Type help for a list of commands.")
 print("To get access to other commands, type getadmin and type the password.")
 print("Type exit to quit.")
 while True:
     cmd = input("$ ")
-    if "sudo" in cmd:
-        open_new_tab("https://sandspiel.club/#d4af60c811854a768c36")
+    if "sudo" in cmd and not isAdmin:
+        print("Sudo is only for admins... rickroll loading...")
+        os.system("start chrome.exe https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley")
     elif cmd == "exit":
         break
     elif cmd == "clear":
-        system("cls")
+        os.system("cls")
     elif cmd == "opc":
         link = input("Enter a link of your choice: ")
-        system("start chrome.exe " + link)
+        os.system("start chrome.exe " + link)
     elif cmd == "opms":
         link = input("Enter a link of your choice: ")
-        system("start msedge.exe " + link)
+        os.system("start msedge.exe " + link)
     elif cmd == "opmz":
         link = input("Enter a link of your choice: ")
-        system("start firefox.exe " + link)
+        os.system("start firefox.exe " + link)
     elif cmd == "help":
         print("Commands: exit, clear, help, banner, ls, getadmin")
         if isAdmin:
@@ -53,7 +52,7 @@ _ _`.    \  |  |  |  /    .'_ _
     elif cmd == "getadmin":
         for count in range(1, 3):
             print("Input password:")
-            p = getpass(prompt="$* ")
+            p = getpass.getpass(prompt="$* ")
             if  hashPassword(p) == adminHash:
                 print("You are an admin!")
                 isAdmin = True
@@ -61,10 +60,10 @@ _ _`.    \  |  |  |  /    .'_ _
             else:
                 print("That is not password. Please try again. " + str(count) + "/3")
     elif cmd == "pong":
-        pong()
+        Games.pong()
     elif cmd == "eldenring":
-        eldenring()
-    elif cmd == "ls":
+        Games.eldenring()
+    elif cmd == "ls" or cmd == "cd":
         ls()
     elif cmd == "games":
         print("Games can be accessed by simple typing their name into the terminal. Eg. eldenring.")
@@ -73,4 +72,4 @@ _ _`.    \  |  |  |  /    .'_ _
         print("Elden Ring (Terminal Edition) - eldenring")
     else:
         print("The command is invalid. Try the help command to get some help.")
-system("cls")
+os.system("cls")
